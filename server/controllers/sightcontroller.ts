62c2df1a6172b7259c3cff88cdf22259399f0694
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-08 15:50:07 +0100

    FEAT: Implement getAllRecords function

---
+++ server/controllers/sightcontroller.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getAllRecords() {
+  async function main() {
+    const sightings = await prisma.personalSighting.findMany();
+    console.log(sightings);
+  }
+  main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  });
+};
+
+getAllRecords();
