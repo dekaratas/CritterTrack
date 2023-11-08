0f3caa442787e82e246d0fbb35808e6ae7211a19
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-08 22:52:55 +0100

    Start work on AddRecord form

---
+++ CritterTrack/src/renderer/src/components/AddRecord/AddRecord.jsx
@@ -0,0 +1,10 @@
+import './AddRecord.css'
+
+export default function AddRecord() {
+  return (
+    <div className="addRecordContainer">
+      <div className="addImage">Image</div>
+      <div className="additionalInfo">Info</div>
+    </div>
+  )
+}
--- CritterTrack/src/renderer/src/components/Dashboard/Dashboard.jsx
+++ CritterTrack/src/renderer/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,12 @@
+import AddRecord from '../AddRecord/AddRecord'
 import Header from '../Header/Header'
 import './dashboard.css'
 
 export default function Dashboard() {
-  return <Header />
+  return (
+    <div className="dashboardContainer">
+      <Header />
+      <AddRecord />
+    </div>
+  )
 }
