cd14412a5bc2b7571af7a412d32c4567fa571ff7
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-09 01:02:59 +0100

    Get input fields in order

--- CritterTrack/src/renderer/src/components/AddRecord/AddRecord.jsx
+++ CritterTrack/src/renderer/src/components/AddRecord/AddRecord.jsx
@@ -1,58 +1,93 @@
 import './AddRecord.css'
+import { useState } from 'react'
 
 export default function AddRecord() {
+  const [inputs, setInputs] = useState([
+    { id: 'vernacular', value: '', name: 'Common Name' },
+    { id: 'date', value: '', name: 'Date' },
+    { id: 'sss', value: '', name: 'Sea Surface Salinity' },
+    { id: 'sst', value: '', name: 'Sea Surface Temperature' },
+    { id: 'shoredistance', value: '', name: 'Shore Distance' },
+    { id: 'depth', value: '', name: 'Depth' },
+    { id: 'count', value: '', name: 'Count' },
+    { id: 'imgURL', value: '', name: 'Image URL' },
+    { id: 'longitude', value: '', name: 'Longitude' },
+    { id: 'latitude', value: '', name: 'Latitude' },
+    { id: 'country', value: '', name: 'Country' }
+  ])
+
+  const handleInputChange = (e, id) => {
+    const updatedInputs = inputs.map((input) => {
+      if (input.id === id) {
+        return { ...input, value: e.target.value }
+      }
+      return input
+    })
+    setInputs(updatedInputs)
+  }
+
   return (
     <div className="addRecordContainer">
-      <div className="addImage">Image</div>
-      <div className="additionalInfo">
-        <form>
-          <label>
-            Common Name:
-            <input type="text" name="vernacular" />
-          </label>
-          <label>
-            Sea Surface Salinity:
-            <input type="number" step="any" name="sss" />
-          </label>
-          <label>
-            Sea Surface Temperature:
-            <input type="number" step="any" name="sst" />
-          </label>
-          <label>
-            Date:
-            <input type="date" name="date" />
-          </label>
-          <label>
-            Shore Distance:
-            <input type="number" name="shoredistance" />
-          </label>
-          <label>
-            Depth:
-            <input type="number" name="depth" />
-          </label>
-          <label>
-            Count:
-            <input type="number" name="count" />
-          </label>
-          <label>
-            Image URL:
-            <input type="text" name="imgURL" />
-          </label>
-          <label>
-            Longitude:
-            <input type="number" step="any" name="longitude" />
-          </label>
-          <label>
-            Latitude:
-            <input type="number" step="any" name="latitude" />
-          </label>
-          <label>
-            Country:
-            <input type="text" name="country" />
-          </label>
-          <button type="submit">Submit</button>
+      <div className="imageContainer">IMAGE</div>
+      <div className="form-container">
+        <form action="">
+          {inputs.map((input) => (
+            <div className="input-container" key={input.id}>
+              <input
+                type={input.id === 'date' ? 'date' : 'text'}
+                id={input.id}
+                required
+                value={input.value}
+                onChange={(e) => handleInputChange(e, input.id)}
+              />
+              <label htmlFor={input.id} className={input.value ? 'float' : ''}>
+                {input.name}
+              </label>
+            </div>
+          ))}
         </form>
       </div>
     </div>
   )
 }
+
+//   <label>
+//   Sea Surface Salinity:
+//   <input type="number" step="any" name="sss" />
+// </label>
+// <label>
+//   Sea Surface Temperature:
+//   <input type="number" step="any" name="sst" />
+// </label>
+// <label>
+//   Date:
+//   <input type="date" name="date" />
+// </label>
+// <label>
+//   Shore Distance:
+//   <input type="number" name="shoredistance" />
+// </label>
+// <label>
+//   Depth:
+//   <input type="number" name="depth" />
+// </label>
+// <label>
+//   Count:
+//   <input type="number" name="count" />
+// </label>
+// <label>
+//   Image URL:
+//   <input type="text" name="imgURL" />
+// </label>
+// <label>
+//   Longitude:
+//   <input type="number" step="any" name="longitude" />
+// </label>
+// <label>
+//   Latitude:
+//   <input type="number" step="any" name="latitude" />
+// </label>
+// <label>
+//   Country:
+//   <input type="text" name="country" />
+// </label>
