248a103e8dd85bcc0bb8f15835f27cc3fcd4565f
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-09 12:42:14 +0100

    Yup, more waves

--- CritterTrack/src/renderer/src/components/Header/Header.jsx
+++ CritterTrack/src/renderer/src/components/Header/Header.jsx
@@ -5,13 +5,21 @@ export default function Header() {
     <div className="headerContainer">
       <h1 className="logo">CritterTrack</h1>
       <h1 className="separator">|</h1>
-      <h1 className="library">My Library</h1>
+      <button>
+        <h1 className="library">My Library</h1>
+      </button>
       <h1 className="separator">|</h1>
-      <h1 className="search">CritterSearch</h1>
+      <button>
+        <h1 className="search">CritterSearch</h1>
+      </button>
       <h1 className="separator">|</h1>
-      <h1 className="addrecord">Add new Record</h1>
+      <button>
+        <h1 className="addrecord">Add new Record</h1>
+      </button>
       <h1 className="separator">|</h1>
-      <h1 className="settings">Settings</h1>
+      <button>
+        <h1 className="settings">Settings</h1>
+      </button>
     </div>
   )
 }
