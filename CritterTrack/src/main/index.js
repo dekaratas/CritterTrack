6b76324690a4f38d41a9f4d69f61b1cb30ced186
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-09 03:59:12 +0100

    Adjust content policy so it allows image display

--- CritterTrack/src/main/index.js
+++ CritterTrack/src/main/index.js
@@ -21,7 +21,8 @@ function createWindow() {
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false,
-      webSecurity: false
+      webSecurity: false,
+      nodeIntegration: true
     }
   })
 
--- CritterTrack/src/renderer/src/components/ImageUpload/ImageUpload.jsx
+++ CritterTrack/src/renderer/src/components/ImageUpload/ImageUpload.jsx
@@ -4,16 +4,19 @@ import { useState } from 'react'
 
 export default function ImageUpload() {
   const [imageUrl, setImageUrl] = useState('')
+
   async function onOpenFileClick() {
     const filePath = await window.electronAPI.openFile()
-    setImageUrl(filePath)
+    const fileUrl = `file://${filePath}`
+    setImageUrl(fileUrl)
+    console.log(filePath)
   }
 
   return (
     <div className="image-upload-container">
       <h1 className="upload-instruct">Upload your image here!</h1>
       <button onClick={onOpenFileClick}>
-        <img src={addImage} alt="imageAdd" />
+        <img src={imageUrl ? imageUrl : addImage} alt="imageAdd" />
         <p>{imageUrl}</p>
       </button>
     </div>
