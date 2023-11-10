db2de5b7908e835bccadcbd9a7af25d7a102c4e3
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-10 20:20:51 +0100

    Test (Success) small NPM package to turn coords into countrycode

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./router.js');
-
+const express = require("express");
+const cors = require("cors");
+const router = require("./router.js");
+const lookup = require("coordinate_to_country");
 
 const app = express();
 const PORT = 3001;
-
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
+console.log(lookup(77.01597, 15.90404));
+
 app.listen(PORT, () => {
   console.log(`🐳 Server running on http://localhost:${PORT} 🐳`);
-})
+});
