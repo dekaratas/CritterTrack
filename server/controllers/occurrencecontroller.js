5693b078a748289bf9b680d0974fbdbbde72880e
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-10 23:02:05 +0100

    Write comments

--- server/controllers/occurrencecontroller.js
+++ server/controllers/occurrencecontroller.js
@@ -1,12 +1,13 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const lookup = require('coordinate_to_country');
-const {countryCodes} = require('../Utils/CountryHashTable.js')
+const lookup = require("coordinate_to_country");
+const { countryCodes } = require("../Utils/CountryHashTable.js");
 
-async function createOccurrence(entry) {
-    const {
+async function createOccurrence(entry, req, res) {
+  const {
     id,
     basisOfRecord,
+    datasetName,
     dataset_id,
     scientificName,
     originalScientificName,
@@ -38,26 +39,37 @@ async function createOccurrence(entry) {
     category,
   } = entry;
 
+  //! To be added sometime this weekend so we can have some actual data to work with on the Front
+  // air_pressure
+  // botTemp
+  // botSal
+  // forkLength
+  // weight and more
+
   try {
+    // Essentially the makeshit place where I push and shove the data we have into a format that Prisma
+    // is happy with, should all be self explanatory
     const parsedEventDate = new Date(eventDate);
     const parsedDateStart = new Date(parseInt(date_start, 10));
     const parsedIndCount = +individualCount;
+    const parsedOrgQuant = +organismQuantity;
 
-    // Intercept times when country is not given because we will ALWAYS have lat and long 
+    // Intercept times when country is not given because we will ALWAYS have lat and long
     let newCountry = country;
 
     if (country == null) {
       const evalCountry = lookup(decimalLatitude, decimalLongitude);
       if (Array.isArray(evalCountry)) {
-       newCountry = countryCodes.get(evalCountry[0]);
+        newCountry = countryCodes.get(evalCountry[0]);
       } else {
-       newCountry = countryCodes.get(evalCountry + "");
+        newCountry = countryCodes.get(evalCountry + "");
       }
     }
     const newOccurrence = await prisma.occurrence.create({
       data: {
         id,
         basisOfRecord,
+        datasetName,
         dataset_id,
         scientificName,
         originalScientificName,
@@ -79,7 +91,7 @@ async function createOccurrence(entry) {
         minimumDepthInMeters,
         maximumDepthInMeters,
         depth,
-        organismQuantity,
+        organismQuantity: parsedOrgQuant,
         individualCount: parsedIndCount,
         recordedBy,
         shoredistance,
@@ -89,21 +101,27 @@ async function createOccurrence(entry) {
         category,
       },
     });
-
-    console.log("New Occurrence created:", newOccurrence);
   } catch (error) {
-    console.error("Error creating Occurrence:", error);
+    console.error("Something went wrong!", error);
   }
 }
 
-async function createOccurrences(apiData) {
-  console.log(apiData);
-  for (const entry of apiData.body) {
-    await createOccurrence(entry);
+// Only 'create' function to be exported in this file since its sole purpose is to create as much data as possible
+// TODO: Fix node complaining about res.send not being a function in my try/catch statement
+async function createOccurrences(apiData, req, res) {
+  try {
+    for (const entry of apiData.body) {
+      await createOccurrence(entry, req, res);
+    }
+    console.log("Great Success!");
+    await prisma.$disconnect();
+  } catch (error) {
+    console.log("Great Failure!");
   }
-  await prisma.$disconnect();
 }
 
+// TODO: Figure out if it's best to use their unique IDs or just go with an incrementing one
+// Deleting via id as endpoint doesn't work when the id is like 00000753-6618-43c7-8875-89a150c39097
 async function deleteOccurrence(req, res) {
   try {
     const { id } = req.params;
@@ -115,11 +133,11 @@ async function deleteOccurrence(req, res) {
     res.status(201).send(entry);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Something went wrong, oops!");
+    res.status(500).send("Failed to remove the entry!");
   }
 }
 
 module.exports = {
   createOccurrences,
-  deleteOccurrence
+  deleteOccurrence,
 };
