356e36c7189380b17baa46c6321b0a48d438c39c
Dennis Karatas <thebr1dgeburner@proton.me>
2023-11-10 21:40:11 +0100

    Mapping Countries to two Coords works now

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router.js");
-const lookup = require("coordinate_to_country");
 
 const app = express();
 const PORT = 3001;
@@ -9,8 +8,6 @@ const PORT = 3001;
 app.use(cors());
 app.use(express.json());
 app.use(router);
-
-console.log(lookup(77.01597, 15.90404));
 
 app.listen(PORT, () => {
   console.log(`🐳 Server running on http://localhost:${PORT} 🐳`);
